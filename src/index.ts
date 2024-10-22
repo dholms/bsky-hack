3c21e1fdc5e0042f7a644f8030c89b372b91ad7d
dholms <dtholmgren@gmail.com>
2024-10-22 18:39:18 -0500

    listen for post message

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,12 @@
-import express from 'express'
-import http from 'node:http'
-import events from 'node:events'
-import { createRoutes } from './routes'
-import { HackConfig } from './config'
-export { readEnv } from './config'
+import dotenv from 'dotenv'
+import { HackServer } from './server'
+import { readEnv } from './config'
 
-export class HackServer {
-  constructor(
-    public app: express.Application,
-    public server: http.Server,
-  ) {}
+dotenv.config()
 
-  static async create(cfg: HackConfig) {
-    const app = express()
-
-    app.use(express.json())
-    app.use(createRoutes(cfg))
-
-    const server = app.listen(cfg.port)
-    await events.once(server, 'listening')
-    console.log('server running')
-
-    return new HackServer(app, server)
-  }
-
-  async destroy() {
-    this.server.close()
-    await events.once(this.server, 'close')
-  }
+const run = async () => {
+  const config = readEnv()
+  await HackServer.create(config)
 }
+
+run()
